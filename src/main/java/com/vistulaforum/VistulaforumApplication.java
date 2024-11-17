defa07721d351308b30041d7d06c47406a4fa0f5
szymczerko <paczuskiszymon16@gmail.com>
2024-11-17 15:40:18 +0100

    initial commit

---
+++ src/main/java/com/vistulaforum/VistulaforumApplication.java
@@ -0,0 +1,13 @@
+package com.vistulaforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VistulaforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VistulaforumApplication.class, args);
+	}
+
+}
